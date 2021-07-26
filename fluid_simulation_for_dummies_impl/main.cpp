44d9e41ad713ef8412801c279a300d6122a3f450
Weikang01 <lwkdota@qq.com>
2021-07-26 18:50:40 -0400

    After lin_solve and diffuse

--- fluid_simulation_for_dummies_impl/main.cpp
+++ fluid_simulation_for_dummies_impl/main.cpp
@@ -94,7 +94,6 @@ void FluidCubeStep(FluidCube* cube)
 	float* density = cube->density;
 
 	/*
-	* TODO:
 	diffuse - Put a drop of soy sauce in some water, and you'll notice that it doesn't stay still, but it spreads out. This happens even if the water and sauce are both perfectly still. This is called diffusion. We use diffusion both in the obvious case of making the dye spread out, and also in the less obvious case of making the velocities of the fluid spread out.
 	*/
 	diffuse(1, Vx0, Vx, visc, dt, 4, N);
@@ -102,13 +101,11 @@ void FluidCubeStep(FluidCube* cube)
 	diffuse(3, Vz0, Vz, visc, dt, 4, N);
 
 	/*
-	* TODO:
 	project - Remember when I said that we're only simulating incompressible fluids? This means that the amount of fluid in each box has to stay constant. That means that the amount of fluid going in has to be exactly equal to the amount of fluid going out. The other operations tend to screw things up so that you get some boxes with a net outflow, and some with a net inflow. This operation runs through all the cells and fixes them up so everything is in equilibrium.
 	*/
 	project(Vx0, Vy0, Vz0, Vx, Vy, 4, N);
 
 	/*
-	* TODO:
 	advect - Every cell has a set of velocities, and these velocities make things move. This is called advection. As with diffusion, advection applies both to the dye and to the velocities themselves.
 	*/
 	advect(1, Vx, Vx0, Vx0, Vy0, Vz0, dt, N);
@@ -119,6 +116,102 @@ void FluidCubeStep(FluidCube* cube)
 
 	diffuse(0, s, density, diff, dt, 4, N);
 	advect(0, density, s, Vx, Vy, Vz, dt, N);
+}
+
+static void set_bnd(int b, float* x, int N)
+{
+	for (int j = 1; j < N - 1; j++)
+	{
+		for (int i = 1; i < N - 1; i++)
+		{
+			x[IX(i, j, 0)] = b == 3 ? -x[IX(i, j, 1)] : x[IX(i, j, 1)];
+			x[IX(i, j, N - 1)] = b == 3 ? -x[IX(i, j, N - 2)] : x[IX(i, j, N - 2)];
+		}
+	}
+	for (int k = 1; k < N - 1; k++)
+	{
+		for (int i = 1; i < N - 1; i++)
+		{
+			x[IX(i, 0, k)] = b == 2 ? -x[IX(i, 1, k)] : x[IX(i, 1, k)];
+			x[IX(i, N - 1, k)] = b == 2 ? -x[IX(i, N - 2, k)] : x[IX(i, N - 2, k)];
+		}
+	}
+	for (int k = 1; k < N - 1; k++)
+	{
+		for (int j = 1; j < N - 1; j++)
+		{
+			x[IX(0, j, k)] = b == 1 ? -x[IX(1, j, k)] : x[IX(1, j, k)];
+			x[IX(N - 1, j, k)] = b == 1 ? -x[IX(N - 2, j, k)] : x[IX(N - 2, j, k)];
+		}
+	}
+
+	x[IX(0, 0, 0)]             = .33f * (x[IX(1, 0, 0)] 
+		                               + x[IX(0, 1, 0)] 
+		                               + x[IX(0, 0, 1)]);
+	x[IX(0, N - 1, 0)]         = .33f * (x[IX(1, N - 1, 0)]
+		                               + x[IX(0, N - 2, 0)]
+		                               + x[IX(0, N - 1, 1)]);
+	x[IX(0, 0, N - 1)]         = .33f * (x[IX(1, 0, N - 1)]
+		                               + x[IX(0, 1, N - 1)]
+		                               + x[IX(0, 0, N - 2)]);
+	x[IX(0, N - 1, N - 1)]     = .33f * (x[IX(1, N - 1, N - 1)]
+		                               + x[IX(0, N - 2, N - 1)]
+		                               + x[IX(0, N - 1, N - 2)]);
+	x[IX(N - 1, 0, 0)]         = .33f * (x[IX(N - 2, 0, 0)]
+		                               + x[IX(N - 1, 1, 0)]
+		                               + x[IX(N - 1, 0, 1)]);
+	x[IX(N - 1, N - 1, 0)]     = .33f * (x[IX(N - 2, N - 1, 0)]
+		                               + x[IX(N - 1, N - 2, 0)]
+		                               + x[IX(N - 1, N - 1, 1)]);
+	x[IX(N - 1, 0, N - 1)]     = .33f * (x[IX(N - 2, 0, N - 1)]
+		                               + x[IX(N - 1, 1, N - 1)]
+		                               + x[IX(N - 1, 0, N - 2)]);
+	x[IX(N - 1, N - 1, N - 1)] = .33f * (x[IX(N - 2, N - 1, N - 1)]
+		                               + x[IX(N - 1, N - 2, N - 1)]
+		                               + x[IX(N - 1, N - 1, N - 2)]);
+}
+
+static void lin_solve(int b, float* x, float* x0, float a, float c, int iter, int N)
+{
+	float cRecip = 1.0f / c;
+	for (int k = 0; k < iter; k++)
+	{
+		for (int m = 1; m < N - 2; m++)
+		{
+			for (int j = 1; j < N - 2; j++)
+			{
+				for (int i = 1; i < N - 2; i++)
+				{
+					x[IX(i, j, m)] = (x0[IX(i, j, m)]
+						+ a * (
+							x[IX(i + 1, j, m)]
+							+ x[IX(i - 1, j, m)]
+							+ x[IX(i, j + 1, m)]
+							+ x[IX(i, j - 1, m)]
+							+ x[IX(i, j, m + 1)]
+							+ x[IX(i, j, m - 1)]
+							)) * cRecip;
+				}
+			}
+		}
+	}
+	set_bnd(b, x, N);
+}
+
+static void diffuse(int b, float* x, float* x0, float diff, float dt, int iter, int N)
+{
+	float a = dt * diff * (N - 2) * (N - 2);
+	lin_solve(b, x, x0, a, 1 + 6 * a, iter, N);
+}
+
+static void project(float* velocX, float* velocY, float* velocZ, float* p, float* div, int iter, int N)
+{
+	// TODO
+}
+
+static void advect(int b, float* d, float* d0, float* velocX, float* velocY, float* velocZ, float dt, int N)
+{
+	// TODO
 }
 
 void main()
